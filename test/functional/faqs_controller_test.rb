53249754631adf8b8e96b7d05b9f658231e862b4
theinterned <ned@theinterned.net>
2009-01-30 21:42:00 +0000

    updated tests to reflect validations

--- test/functional/faqs_controller_test.rb
+++ test/functional/faqs_controller_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require 'test/test_helper'
 
 class FaqsControllerTest < ActionController::TestCase
   test "should get index" do
@@ -12,12 +12,25 @@ class FaqsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create faq" do
+  test "should create faq with question and answer" do
     assert_difference('Faq.count') do
-      post :create, :faq => { }
+      post :create, :faq => {:question => "what?", :answer => "exactly!"}
     end
-
     assert_redirected_to faq_path(assigns(:faq))
+  end
+  
+  test "should not create faq without question" do
+    assert_no_difference('Faq.count') do
+      post :create, :faq => {:answer => "exactly!"}
+    end
+    assert_response :success 
+  end
+
+  test "should not create faq without answer" do
+    assert_no_difference('Faq.count') do
+      post :create, :faq => {:answer => "exactly!"}
+    end
+    assert_response :success
   end
 
   test "should show faq" do
--- test/unit/faq_test.rb
+++ test/unit/faq_test.rb
@@ -1,8 +1,18 @@
-require 'test_helper'
+require 'test/test_helper'
 
 class FaqTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
+  end
+  
+  test "faq should not save without question" do
+    faq = Faq.new(:answer => "test answer")
+    assert !faq.save, "Saved the FAQ without a question"
+  end
+  
+  test "faq should not save without answer" do
+    faq = Faq.new(:question => "test question")
+    assert !faq.save, "Saved the FAQ without a answer"
   end
 end
