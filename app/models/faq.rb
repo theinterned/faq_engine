9ec67126e9eaf7e0a74986e41b468059e861a30e
theinterned <ned@theinterned.net>
2009-02-03 14:55:04 +0000

    added tagging to the faq model and a unit test for it

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,3 +1,4 @@
 class Faq < ActiveRecord::Base
   validates_presence_of :question, :answer
+  acts_as_taggable
 end
--- test/unit/faq_test.rb
+++ test/unit/faq_test.rb
@@ -6,6 +6,11 @@ class FaqTest < ActiveSupport::TestCase
     assert true
   end
   
+  test "faq should save wiht question and answer provided" do
+    faq = Faq.new(:question => "What?", :answer => "I don't know!")
+    assert faq.save
+  end
+  
   test "faq should not save without question" do
     faq = Faq.new(:answer => "test answer")
     assert !faq.save, "Saved the FAQ without a question"
@@ -14,5 +19,11 @@ class FaqTest < ActiveSupport::TestCase
   test "faq should not save without answer" do
     faq = Faq.new(:question => "test question")
     assert !faq.save, "Saved the FAQ without a answer"
+  end
+
+  test "faq should save csv tag string as an array" do
+    faq = Faq.new(:question => "What?", :answer => "I don't know!", :tag_list => "Tag One, Tag Two, Tag Three")
+    assert_equal faq.tag_list, ["Tag One", "Tag Two", "Tag Three"]
+    assert faq.save    
   end
 end
